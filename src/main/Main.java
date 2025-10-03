1ede04beaa75904e88573dd915402c2ca3bd1d69
Xionexilem <lev.mitrakov@bk.ru>
2025-10-03 14:42:50 +0500

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
   // Задание 1
   // Задача 2
   public int sumLastNums (int x) {
-    int first = x%10;
-    int second = (x/10)%10;
+    int first = x % 10;
+    int second = (x / 10) % 10;
 
     return first + second;
   }
@@ -36,13 +38,13 @@ public class Main {
     if (x > 0) {
       return x;
     } else {
-      return x*-1;
+      return x * -1;
     }
   }
 
   // Задача 2
   public double safeDiv (int x, int y) {
-    if (y==0) {
+    if (y == 0) {
       return 0;
     } else {
       return (double) x/y;
@@ -50,84 +52,162 @@ public class Main {
   }
 
   // Задача 7
-  public boolean isInRange (int a, int b, int num) {
-    if (a > b) {
-      return a >= num && b <= num;
+  public int sum2 (int x, int y) {
+    int summa = x + y;
+    if (10 <= summa && summa <= 19) {
+      return 20;
     } else {
-      return a <= num && b >= num;
+      return summa;
     }
   }
 
   // Задача 8
-  public boolean isDivisor (int a, int b)
+  public String age (int x) {
+    int remainder = x % 10;
+    int tens = x / 10 % 10;
+    if (remainder == 1 && tens != 1) {
+      return x + " год";
+    } else if (2 <= remainder && remainder <= 4 && tens != 1) {
+      return x + " года";
+    } else {
+      return x + " лет";
+    }
+  }
+
+  // Задача 9
+  public String day (int x) {
+    switch (x) {
+      case 1:
+        return "понедельник";
+      case 2:
+        return "вторник";
+      case 3:
+        return "среда";
+      case 4:
+        return "четверг";
+      case 5:
+        return "пятница";
+      case 6:
+        return "суббота";
+      case 7:
+        return "воскресенье";
+      default:
+        return "это не день недели";
+    }
+  }
+
   // Задание 3
-  // Задача 1
-  public String listNums (int x) {
-    StringBuilder numbers = new StringBuilder();
-    for (int i = 0; i <= x; i++) {
-      numbers.append(i);
-      if (i < x) {
-        numbers.append(" ");
-      }
-    }
-    return numbers.toString();
-  }
-
-  // Задача 2
-  public String reverseListNums (int x) {
-    StringBuilder numbers = new StringBuilder();
-    for (int i = x; i >= 0; i--) {
-      numbers.append(i);
-      if (i > 0) {
-        numbers.append(" ");
-      }
-    }
-    return numbers.toString();
-  }
-
   // Задача 3
   public String chet (int x) {
-    StringBuilder numbers = new StringBuilder();
+    StringBuilder result = new StringBuilder();
     for (int i = 0; i <= x; i++) {
-      numbers.append(i);
-      if (i%2==0) {
-        if (i < x) {
-          numbers.append(" ");
+      if (i % 2 == 0) {
+        result.append(i);
+        if (i != x) {
+          result.append(" ");
         }
       }
     }
-    return numbers.toString();
-  }
-
-  // Задача 4
-  public int pow (int x, int y) {
-    int result = 1;
-    for (int i = 0; i < y; i++) {
-        result *= x;
-      }
-    return result;
+    return result.toString();
   }
 
   // Задача 5
   public int numLen (long x) {
-    int count = 0;
-    while (x > 0) {
+    int result = 0;
+    while (x != 0) {
       x /= 10;
-      count += 1;
+      result++;
     }
-    return count;
+    return result;
+  }
+
+  // Задача 7
+  public void square (int x) {
+    for (int i = 0; i < x; i++) {
+      for (int j = 0; j < x; j++) {
+        System.out.print('*');
+      }
+      System.out.println("");
+    }
+  }
+
+  // Задача 8
+  public void leftTriangle (int x) {
+    for (int i = 1; i <= x; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.print('*');
+      }
+      System.out.println("");
+    }
+  }
+
+  // Задача 9
+  public void rightTriangle (int x) {
+    for (int i = x; i > 0; i--) {
+      for (int j = 1; j <= x; j++) {
+        if (j < i) {
+          System.out.print(' ');
+        } else {
+          System.out.print('*');
+        }
+      }
+      System.out.println("");
+    }
   }
 
   // Задание 4
-  // Задача 1
-  public int findFirst (int[] arr, int x) {
-    int result = -1;
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i]==x) {
-        result = i;
-        break;
-      }
+  // Задача 4
+  public int[] add (int[] arr, int x, int pos) {
+    int[] newArr = new int[arr.length + 1];
+    for (int i = 0; i < pos; i++) {
+      newArr[i] = arr[i];
     }
-    return result;
+    newArr[pos] = x;
+    for (int i = pos; i < arr.length; i++) {
+      newArr[i + 1] = arr[i];
+    } 
+    return newArr;
+  }
+
+  // Задача 5
+  public int[] add (int[] arr, int[] ins, int pos) {
+    int[] newArr = new int[arr.length + ins.length];
+    for (int i = 0; i < pos; i++) {
+      newArr[i] = arr[i];
+    }
+    for (int i = 0; i < ins.length; i++) {
+      newArr[i + pos] = ins[i];
+    }
+    for (int i = pos; i < arr.length; i++) {
+      newArr[i + arr.length] = arr[i]; 
+    }
+    return newArr;
+  }
+
+  // Задача 6
+  public void reverse (int[] arr) {
+    for (int i = 0; i < arr.length / 2; i++) {
+      int temp = arr[i];
+      int index = arr.length - 1 - i;
+      arr[i] = arr[index];
+      arr[index] = temp;
+    }
+  }
+
+  private void visualTopTab (String exercise, String task) {
+    String visual = " ========== ";
+    System.out.println(visual + "Задание №" + exercise + " Задача №" + task + visual);
+  }
+
+  private void visualBottomTab () {
+    String visual = " ========== ";
+    System.out.println(visual + "Конец" + visual);
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String numberExercise = scanner.nextInt();
+    String numberTask = scanner.nextLine();
+
   }
 }
